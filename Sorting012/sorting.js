8a154d0bb2283c1b716d2e87cd9cebcf643f0e7a
Oviya Rangasamy <oviya.rangasamy@cesltd.com>
2022-12-09 22:00:36 +0530

    added the code

--- Sorting012/sorting.js
+++ Sorting012/sorting.js
@@ -11,4 +11,26 @@
  * return the merged array
  */
 
+function SortArray(arr){
+    let val0 = []
+    let val1 = []
+    let val2 = []
+    for (let i of arr){
+        if(i === 0){
+            val0.push(i)
+        }
+        else if(i=== 1){
+            val1.push(i)
+        }
+        else{
+            val2.push(i)
+        }
+    }
+    let finalArray = [...val0,...val1,...val2]
+    return finalArray
+}
+
+console.log(SortArray([1,2,1,1,1,0,0,0,1,2]))
+console.log(SortArray([1,0,1]))
+console.log(SortArray([2,0,1,2]))
 
