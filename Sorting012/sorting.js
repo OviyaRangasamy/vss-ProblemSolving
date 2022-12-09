57857291ee5885ecb626d3cfbb3b03e3d87e6c24
Oviya Rangasamy <oviya.rangasamy@cesltd.com>
2022-12-09 21:32:26 +0530

    Algorithm for sorting

---
+++ Sorting012/sorting.js
@@ -0,0 +1,14 @@
+/**
+ * create a function with the array parameter
+ * create an empty array for 0s
+ * create an empty array for 1s
+ * create an empty array for 2s
+ * iterate the input array
+ * if value= 0 push it to the 0s array
+ * if value= 1 push it to the 1s array
+ * if value = 2 push it to the 2s array
+ * after the iteration completed merge the arrays in order of 0,1,2
+ * return the merged array
+ */
+
+
